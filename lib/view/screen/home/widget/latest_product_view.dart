0d36e40816526b05e7942c99276a70ff57edfa32
ahmedsaad12 <ahmedmohamed23345@gmail.com>
2023-05-21 13:01:17 +0300

    ch

--- lib/view/screen/home/widget/latest_product_view.dart
+++ lib/view/screen/home/widget/latest_product_view.dart
@@ -8,26 +8,33 @@ import 'package:provider/provider.dart';
 
 class LatestProductView extends StatelessWidget {
   final ScrollController scrollController;
-  LatestProductView({ this.scrollController});
+
+  LatestProductView({this.scrollController});
 
   @override
   Widget build(BuildContext context) {
     int offset = 1;
     scrollController?.addListener(() {
-      if(scrollController.position.maxScrollExtent == scrollController.position.pixels
-          && Provider.of<ProductProvider>(context, listen: false).lProductList.length != 0
-          && !Provider.of<ProductProvider>(context, listen: false).isLoading) {
+      if (scrollController.position.maxScrollExtent ==
+              scrollController.position.pixels &&
+          Provider.of<ProductProvider>(context, listen: false)
+                  .lProductList
+                  .length !=
+              0 &&
+          !Provider.of<ProductProvider>(context, listen: false).isLoading) {
         int pageSize;
-        pageSize = Provider.of<ProductProvider>(context, listen: false).lPageSize;
+        pageSize =
+            Provider.of<ProductProvider>(context, listen: false).lPageSize;
 
-        if(offset < pageSize) {
+        if (offset < pageSize) {
           offset++;
           print('end of the page');
-          Provider.of<ProductProvider>(context, listen: false).showBottomLoader();
-          Provider.of<ProductProvider>(context, listen: false).getLatestProductList(offset, context);
+          Provider.of<ProductProvider>(context, listen: false)
+              .showBottomLoader();
+          Provider.of<ProductProvider>(context, listen: false)
+              .getLatestProductList(offset, context);
         }
       }
-
     });
 
     return Consumer<ProductProvider>(
@@ -36,27 +43,30 @@ class LatestProductView extends StatelessWidget {
         productList = prodProvider.lProductList;
 
         return Column(children: [
-
-          !prodProvider.firstLoading ? productList.length != 0 ?
-          Container(
-            height: Dimensions.CARD_HEIGHT,
-            child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: productList.length,
-                itemBuilder: (ctx,index){
-                  return Container(width: (MediaQuery.of(context).size.width/2)-20,
-                      child: ProductWidget(productModel: productList[index]));
-
-                }),
-          ): SizedBox.shrink() : ProductShimmer(isHomePage: true ,isEnabled: prodProvider.firstLoading),
+          !prodProvider.firstLoading
+              ? productList.length != 0
+                  ? Container(
+                      height: Dimensions.CARD_HEIGHT,
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: productList.length,
+                          itemBuilder: (ctx, index) {
+                            return Container(
+                                width: (MediaQuery.of(context).size.width / 2) -
+                                    20,
+                                child: ProductWidget(
+                                    productModel: productList[index]));
+                          }),
+                    )
+                  : SizedBox.shrink()
+              : ProductShimmer(
+                  isHomePage: true, isEnabled: prodProvider.firstLoading),
           // prodProvider.isLoading ? Center(child: Padding(
           //   padding: EdgeInsets.all(Dimensions.ICON_SIZE_EXTRA_SMALL),
           //   child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Theme.of(context).primaryColor)),
           // )) : SizedBox.shrink(),
-
         ]);
       },
     );
   }
 }
-
